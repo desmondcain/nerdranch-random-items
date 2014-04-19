f71bfc3c22309913c3efec34f1cb7d16382898d2
desmondcain <desmondcain@gmail.com>
2014-04-19 15:26:21 -0500

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Desmond on 4/19/14.
+//  Copyright (c) 2014 Desmond Cain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
