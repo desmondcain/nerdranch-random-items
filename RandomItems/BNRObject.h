1f23d36a6f90d2e752abedfb8593b24cf5eb67ee
desmondcain <desmondcain@gmail.com>
2014-04-20 14:55:29 -0500

    Created BNRItem sublcass of NSObject.

---
+++ RandomItems/BNRObject.h
@@ -0,0 +1,13 @@
+//
+//  BNRObject.h
+//  RandomItems
+//
+//  Created by Desmond on 4/20/14.
+//  Copyright (c) 2014 Desmond Cain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRObject : NSObject
+
+@end
---
+++ RandomItems/BNRObject.m
@@ -0,0 +1,13 @@
+//
+//  BNRObject.m
+//  RandomItems
+//
+//  Created by Desmond on 4/20/14.
+//  Copyright (c) 2014 Desmond Cain. All rights reserved.
+//
+
+#import "BNRObject.h"
+
+@implementation BNRObject
+
+@end
