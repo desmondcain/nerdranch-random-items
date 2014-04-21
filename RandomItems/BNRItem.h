9b0d44ec07193f0ccb5dc18c601469c799043bab
desmondcain <desmondcain@gmail.com>
2014-04-20 21:21:31 -0500

    Declared accessor methods for the instance variables declared in the BNRItem class.

--- RandomItems/BNRItem.h
+++ RandomItems/BNRItem.h
@@ -9,5 +9,22 @@
 #import <Foundation/Foundation.h>
 
 @interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+}
+
+- (void)setItemName:(NSString *)str;
+- (NSString *)itemName;
+
+- (void)setSerialNumber:(NSString *)str;
+- (NSString *)serialNumber;
+
+- (void)setValueInDollars:(int)v;
+- (int)setValueInDollars;
+
+- (NSData *)dateCreated;
 
 @end
